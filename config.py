dad78786074b5eb455f58cebcd87fe14c520626b
Yang Mingxin <ymx@hust.edu.cn>
2024-03-22 18:58:00 +0800

    new_init

---
+++ config.py
@@ -0,0 +1,15 @@
+# Description: This file contains the path to the models and the temperature and top_p values for the generation of the conversers.
+
+VICUNA_PATH = "/model/BJiao/vicuna-13b-v1-5/vicuna-13b-v1.5" #TODO: add path
+LLAMA_PATH = "/model/BJiao/CodeLlama-7b-Instruct-hf/CodeLlama-7b-Instruct-hf" #TODO: add path
+DEEPSEEK_PATH = "/model/BJiao/deepseek-coder-7b-instru/deepseek-coder-7b-instruct-v1.5" #TODO: add path
+CODELLAMA_PATH = "/home/fnii/workspace/codellama-7b" #TODO: add path
+QWEN_PATH = "/model/BJiao/Qwen1-5-7B-Chat/Qwen1.5-7B-Chat" #TODO: add path
+CHATGLM3_PATH = "/model/BJiao/chatglm3-6b/chatglm3-6b" #TODO: add path
+
+
+PEFT_PATH = f'/home/fnii/workspace/finetune/scripts/src/output/checkpoint-200'
+
+
+TEMP = 0.1
+TOP_P = 0.9
